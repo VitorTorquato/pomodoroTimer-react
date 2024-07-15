0ae7f7f96dd5664e4f1b51ff249a74c155ce56ed
vitor torquato <torquatovitor68@gmail.com>
2024-07-15 12:09:50 +0100

    inputs upgrade

--- src/pages/history/History.tsx
+++ src/pages/history/History.tsx
@@ -1,9 +1,75 @@
+import { HistoryContainer ,HistoryList, Status} from "./styles";
 
 
 
 export function History(){
 
     return (
-        <h1>History</h1>
+
+        <HistoryContainer>
+            <h1>Meu histórico</h1>
+
+            <HistoryList>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tarefa</th>
+                            <th>Duracão</th>
+                            <th>Inicio</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Ha 2 meses</td>
+                            <td>
+                                <Status statusColor="green">
+                                    Concluido
+                                </Status>
+                            </td>
+
+                        </tr>    
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Ha 2 meses</td>
+                            <td>
+                                <Status statusColor="yellow">
+                                    Em andamento
+                                </Status>
+                            </td>
+
+                        </tr>    
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Ha 2 meses</td>
+                            <td>
+                                <Status statusColor="red">
+                                    Interrompido
+                                </Status>
+                            </td>
+
+                        </tr>    
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Ha 2 meses</td>
+                            <td>
+                                <Status statusColor="green">
+                                    Concluido
+                                </Status>
+                            </td>
+
+                        </tr>    
+                        
+                    </tbody>    
+                </table>
+            </HistoryList>
+        </HistoryContainer>
+
     )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -13,12 +13,25 @@ export function Home(){
                         <label htmlFor="task">Vou trabalhar em</label>    
                         <TaskInput 
                         placeholder="Qual o nome do seu projeto" 
-                        id="task" />
+                        id="task" 
+                        list="task-suggestions"
+                        />
+
+                        <datalist id="task-suggestions">
+                            <option value="projeto 1"/>
+                            <option value="projeto 2"/>
+                            <option value="projeto 3"/>
+                            <option value="projeto 4"/>
+                        </datalist>
 
                         <label htmlFor="minutesAmount">Durante</label>
                         <MinutesAmount
+                        placeholder="00"
                         type="number"
-                        id="minutesAmount"/>
+                        id="minutesAmount"
+                        step={5}
+                        min={5}
+                        max={60}/>
 
                         <span>minutos.</span>
                     </FormContainer>                
