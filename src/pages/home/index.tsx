8977722a20e817e3646e721e5d35f50c215f7132
vitor torquato <torquatovitor68@gmail.com>
2024-07-17 14:23:31 +0100

    react hook form install

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,20 +1,53 @@
-import { Play } from "@phosphor-icons/react"
-import { HomeContainer ,FormContainer , CountdownContainer, Separator, StartCountdownBtn, TaskInput, MinutesAmount} from "./styles"
+import { Play } from "@phosphor-icons/react";
+import { useForm } from 'react-hook-form';
+import { HomeContainer ,FormContainer , CountdownContainer, Separator, StartCountdownBtn, TaskInput, MinutesAmount} from "./styles";
+
+
+//contolled : manter em tempo real a inforacao dos inputs no estado da aplicacao dentro do estado.
+// uncontrolled: buscar a informacao do valor do input somente quando precisarmos dela *mais usado em formularios muito grandes, pois nao vale a pena renderizar o componente a cada mudanca ou a cada tecla digitada.
+
+
+
+/**
+ function register(name: string){
+ retur(
+
+        onChange () => void,
+           onBlur () => void 
+ )
+ 
+ }
+
+* */
 
 
 export function Home(){
+        
+    const {register , handleSubmit , watch} = useForm();
 
+
+    function handleCreateNewCycle(data: any){
+            console.log(data)
+    }
+
+    const task = watch('task')
+
+
+    //variavel auxiliar
+    const isSubmitDisable = !task
 
 
     return(
         <HomeContainer>
-            <form action="">
+            <form  onSubmit={handleSubmit(handleCreateNewCycle)} action="">
                     <FormContainer>
-                        <label htmlFor="task">Vou trabalhar em</label>    
+                        <label htmlFor="task">Vou trabalhar em</label>
+
                         <TaskInput 
                         placeholder="Qual o nome do seu projeto" 
                         id="task" 
                         list="task-suggestions"
+                        {...register('task')}
                         />
 
                         <datalist id="task-suggestions">
@@ -31,7 +64,9 @@ export function Home(){
                         id="minutesAmount"
                         step={5}
                         min={5}
-                        max={60}/>
+                        max={60}
+                        {...register('minutesAmount' , {valueAsNumber: true})}
+                        />
 
                         <span>minutos.</span>
                     </FormContainer>                
@@ -47,7 +82,7 @@ export function Home(){
 
 
                     <StartCountdownBtn
-                    disabled
+                        disabled={isSubmitDisable}
                         type="submit"
                     >
                         <Play/>Start
